5855c4e6064dd3c6e8808d72e4647c4772c5ee69
kiba0510 <kiba0510@gmail.com>
2021-09-20 14:03:02 -0500

    🔐🔄 Closure and loops 🔐🔄

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,21 @@
+// 🔐🔄 Closure and loops 🔐🔄
+
+function createClassRoom(numbersOfStudents) {
+
+    function studentSeat(seat) {
+        return function() {
+            return seat
+        }
+    }
+    let students = [];
+    for (let i = 0; i < numbersOfStudents; i++) {
+        students[i] = studentSeat (i + 1);
+    }
+    return students
+}
+
+const classRoom = createClassRoom(10);
+
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
