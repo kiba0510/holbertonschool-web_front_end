2b9cdef3c0af6053a14c690fb34238097be6c184
kiba0510 <kiba0510@gmail.com>
2021-09-21 10:31:48 -0500

    Binding

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,12 @@
+// Binding
+
+let roomDimensions = {
+    width: 50,
+    length: 100,
+    getArea() {
+        return this.length * this.width
+    }
+}
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+console.log(boundGetArea());
